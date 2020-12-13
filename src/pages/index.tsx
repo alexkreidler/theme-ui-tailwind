7e3f33931160da229f92ddb84bb99ea1b9a299f5
Alex Kreidler <alexkreidler2020@gmail.com>
2020-12-13 15:45:38 -0500

    add dynamic layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import { ImageCard } from "components/ImageCard";
 import {
@@ -13,6 +13,9 @@ import {
   Heading,
   Message,
   Link,
+  Label,
+  Checkbox,
+  Flex,
 } from "theme-ui";
 import { NavBar } from "components/NavBar";
 import { DemoForm } from "components/DemoForm";
@@ -60,50 +63,94 @@ const demos = [
   },
 ];
 
-const Home: React.FC = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600;700;800;900&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <Box p={4}>
-      <Heading sx={{ pb: 3 }}>Demo Components</Heading>
-      <Text>
-        This is a demo of some{" "}
-        <Link href="https://theme-ui.com/components">Theme UI components</Link>{" "}
-        using a{" "}
-        <Link href="https://github.com/system-ui/theme-ui/pull/1243">
-          TailwindCSS Theme
-        </Link>
-        . It also uses the new Babel/react{" "}
-        <Link href="https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html">
-          automatic JSX runtime feature
-        </Link>
-        , which is{" "}
-        <Link href="https://github.com/system-ui/theme-ui/blob/develop/CHANGELOG.md#v050-alpha1-2020-11-26">
-          new
-        </Link>{" "}
-        to Theme UI.
-      </Text>
-      {demos
-        .filter((d) => !d.hide)
-        .map((d) => (
-          <Box key={d.name} my={4}>
-            <Heading as="h3" sx={{ pb: 2 }}>
-              {d.name}
-            </Heading>
-            {d.c}
-          </Box>
-        ))}
-      {/* <Grid gap={4} columns="1fr 1fr">
-      </Grid> */}
+const demoContent = demos
+  .filter((d) => !d.hide)
+  .map((d) => (
+    <Box key={d.name} my={4}>
+      <Heading as="h3" sx={{ pb: 2 }}>
+        {d.name}
+      </Heading>
+      {d.c}
     </Box>
-  </div>
-);
+  ));
+
+const Home: React.FC = () => {
+  const [container, setContainer] = useState(true);
+  const [grid, setGrid] = useState(false);
+
+  const gWrapped = grid ? (
+    <Grid gap={4} columns="1fr 1fr">
+      {demoContent}
+    </Grid>
+  ) : (
+    demoContent
+  );
+  const cWrapped = container ? (
+    <Container px={4}>{gWrapped}</Container>
+  ) : (
+    <Box p={4}>{gWrapped}</Box>
+  );
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Container sx={{ px: 4, pt: 4 }}>
+        <Heading sx={{ pb: 3 }}>Demo Components</Heading>
+        <Text>
+          This is a demo of some{" "}
+          <Link href="https://theme-ui.com/components">
+            Theme UI components
+          </Link>{" "}
+          using a{" "}
+          <Link href="https://github.com/system-ui/theme-ui/pull/1243">
+            TailwindCSS Theme
+          </Link>
+          . It also uses the new Babel/react{" "}
+          <Link href="https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html">
+            automatic JSX runtime feature
+          </Link>
+          , which is{" "}
+          <Link href="https://github.com/system-ui/theme-ui/blob/develop/CHANGELOG.md#v050-alpha1-2020-11-26">
+            new
+          </Link>{" "}
+          to Theme UI.
+        </Text>
+
+        <Flex mt={3}>
+          <Label>
+            <Checkbox
+              checked={container}
+              onChange={(e) => setContainer(e.target.checked)}
+            />
+            Use <code sx={{ ml: 2 }}>{"<Container/>"}</code>
+          </Label>
+          <Label>
+            <Checkbox
+              checked={grid}
+              onChange={(e) => {
+                console.log(e.target.checked);
+
+                setGrid(e.target.checked);
+              }}
+            />
+            Use <code sx={{ ml: 2 }}>{"<Grid/>"}</code>
+          </Label>
+        </Flex>
+        <Text as="p" mt={2}>
+          The above options allow you to modify the layout of the demo
+          components.
+        </Text>
+      </Container>
+      {cWrapped}
+    </div>
+  );
+};
 
 export default Home;
